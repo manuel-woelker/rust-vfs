fdfd3b29ab4dceae70132aeb05ec2e5b7c18c919
Manuel Woelker <github@manuel.woelker.org>
2020-08-01 12:44:57 +0200

    refactor(impls): move FileSystem implementations to their own module

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@
 //! The virtual file system abstraction generalizes over file systems and allow using
 //! different VirtualFileSystem implementations (i.e. an in memory implementation for unit tests)
 //!
-//! The main interaction with the virtual filesystem is by using virtual paths ([`VfsPath`](struct.VfsPath.html)).
+//! The main interaction with the virtual filesystem is by using virtual paths ([`VfsPath`](path/struct.VfsPath.html)).
 //!
 //! This crate currently has the following implementations:
 //!
-//!  * **[`PhysicalFS`](physical/struct.PhysicalFS.html)** - the actual filesystem of the underlying OS
-//!  * **[`MemoryFS`](memory/struct.MemoryFS.html)** - an ephemeral in-memory implementation (intended for unit tests)
+//!  * **[`PhysicalFS`](impls/physical/struct.PhysicalFS.html)** - the actual filesystem of the underlying OS
+//!  * **[`MemoryFS`](impls/memory/struct.MemoryFS.html)** - an ephemeral in-memory implementation (intended for unit tests)
 
 #[cfg(test)]
 #[macro_use]
@@ -16,12 +16,11 @@ pub mod test_macros;
 
 pub mod error;
 pub mod filesystem;
-pub mod memory;
+pub mod impls;
 pub mod path;
-pub mod physical;
 
 pub use error::{VfsError, VfsResult};
 pub use filesystem::FileSystem;
-pub use memory::MemoryFS;
+pub use impls::memory::MemoryFS;
+pub use impls::physical::PhysicalFS;
 pub use path::*;
-pub use physical::PhysicalFS;
