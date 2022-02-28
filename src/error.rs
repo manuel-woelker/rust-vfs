2a7309c162319e746fb30eacd31fe86ad6adeb2b
Manuel Woelker <github@manuel.woelker.org>
2022-02-28 23:21:26 +0100

    refactor(error): remove dependency on thiserror to reduce dependencies and improve compile times (fixes #25)

--- src/error.rs
+++ src/error.rs
@@ -1,54 +1,78 @@
 //! Error and Result definitions
 
-use std::fmt::Display;
-use thiserror::Error;
+use std::fmt::{Display, Formatter};
+use std::io::Error;
 
 /// The error type of this crate
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum VfsError {
     /// A generic IO error
-    #[error("IO error: {0}")]
-    IoError(#[from] std::io::Error),
+    IoError(std::io::Error),
 
     /// The file or directory at the given path could not be found
-    #[error("The file or directory `{path}` could not be found")]
     FileNotFound {
         /// The path of the file not found
         path: String,
     },
 
     /// The given path is invalid, e.g. because contains '.' or '..'
-    #[error("The path `{path}` is invalid")]
     InvalidPath {
         /// The invalid path
         path: String,
     },
 
     /// Generic error variant
-    #[error("FileSystem error: {message}")]
     Other {
         /// The generic error message
         message: String,
     },
 
     /// Generic error context, used for adding context to an error (like a path)
-    #[error("{context}, cause: {cause}")]
     WithContext {
         /// The context error message
         context: String,
         /// The underlying error
-        #[source]
         cause: Box<VfsError>,
     },
 
     /// Functionality not supported by this filesystem
-    #[error("Functionality not supported by this filesystem")]
     NotSupported,
 }
 
 impl From<String> for VfsError {
     fn from(message: String) -> Self {
         VfsError::Other { message }
+    }
+}
+
+impl From<std::io::Error> for VfsError {
+    fn from(cause: Error) -> Self {
+        VfsError::IoError(cause)
+    }
+}
+
+impl std::fmt::Display for VfsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            VfsError::IoError(cause) => {
+                write!(f, "IO error: {}", cause)
+            }
+            VfsError::FileNotFound { path } => {
+                write!(f, "The file or directory `{}` could not be found", path)
+            }
+            VfsError::InvalidPath { path } => {
+                write!(f, "The path `{}` is invalid", path)
+            }
+            VfsError::Other { message } => {
+                write!(f, "FileSystem error: {}", message)
+            }
+            VfsError::WithContext { context, cause } => {
+                write!(f, "{}, cause: {}", context, cause)
+            }
+            VfsError::NotSupported => {
+                write!(f, "Functionality not supported by this filesystem")
+            }
+        }
     }
 }
 
